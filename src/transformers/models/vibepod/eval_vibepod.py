d00a4ba6aa193fd1f125337a61664c8ef5625c79
pengzhiliang <1083127130@qq.com>
2025-06-12 02:52:05 +0000

    for zh

--- src/transformers/models/vibepod/eval_vibepod.py
+++ src/transformers/models/vibepod/eval_vibepod.py
@@ -72,34 +72,65 @@ def main():
     save_names = []
     scripts = []
     voice_samples = []
-    with open(args.scp_path, 'r') as f:
-        scp_lines = f.readlines()
-        # deepseekR1 /mnt/conversationhub/zhiliang/exp/podcast_eval/select_mosset/transcripts/simulated_sample/deepseekR1.json
-        for line in scp_lines:
-            save_name, json_path = line.strip().split()
-            print(f"Processing file: {json_path}")
-            save_names.append(save_name)
-            with open(json_path, 'r') as json_file:
-                data = json.load(json_file)
+    if args.scp_path.endswith('.json'):
+        # If the input is a single JSON 
+        save_names.append(os.path.basename(args.scp_path).split('.')[0])
+        with open(args.scp_path, 'r') as json_file:
+            data = json.load(json_file)
 
-                prompts = data['prompts']
-                sample_voices = []
-                # Sort keys by converting to integers and get audio paths in order
-                for k in sorted(prompts.keys(), key=int):
-                    sample_voices.append(prompts[k]['audio_path'])
-                voice_samples.append(sample_voices)
+            prompts = data['prompts']
+            sample_voices = []
+            # Sort keys by converting to integers and get audio paths in order
+            for k in sorted(prompts.keys(), key=int):
+                sample_voices.append(prompts[k]['audio_path'])
+            voice_samples.append(sample_voices)
 
-                transcript = data['transcript']
-                sample_scripts = []
-                for sample_sentence in transcript:
-                    speaker_id = sample_sentence['speaker']
-                    sample_scripts.append(f"Speaker {speaker_id}: {sample_sentence['text']}")
-                scripts.append('\n'.join(sample_scripts))
+            transcript = data['transcript']
+            sample_scripts = []
+            for sample_sentence in transcript:
+                speaker_id = sample_sentence['speaker']
+                sample_scripts.append(f"Speaker {speaker_id}: {sample_sentence['text']}")
+            scripts.append('\n'.join(sample_scripts))
+    else:
+        with open(args.scp_path, 'r') as f:
+            scp_lines = f.readlines()
+            # deepseekR1 /mnt/conversationhub/zhiliang/exp/podcast_eval/select_mosset/transcripts/simulated_sample/deepseekR1.json
+            for line in scp_lines:
+                parts = line.strip().split()
+                if len(parts) != 2:
+                    print(f"Skipping invalid line: {line.strip()}")
+                    continue
+                    
+                save_name, json_path = parts
+                print(f"Processing file: {json_path}")
+                # Check if file exists and is readable
+                if not os.path.exists(json_path):
+                    breakpoint()
+                    print(f"File not found: {json_path}")
+                    continue
+
+                save_names.append(save_name)
+                with open(json_path, 'r') as json_file:
+                    data = json.load(json_file)
+
+                    prompts = data['prompts']
+                    sample_voices = []
+                    # Sort keys by converting to integers and get audio paths in order
+                    for k in sorted(prompts.keys(), key=int):
+                        sample_voices.append(prompts[k]['audio_path'])
+                    voice_samples.append(sample_voices)
+
+                    transcript = data['transcript']
+                    sample_scripts = []
+                    for sample_sentence in transcript:
+                        speaker_id = sample_sentence['speaker']
+                        sample_scripts.append(f"Speaker {speaker_id}: {sample_sentence['text']}")
+                    scripts.append('\n'.join(sample_scripts))
     
     # Load processor
     print(f"Loading processor & model from {args.model_path}")
     processor = VibePodProcessor.from_pretrained(
-        '/tmp/vibepod-model',
+        args.model_path,
         cache_dir="/mnt/msranlp/zliang/hf_ckpt"
     )
 
@@ -122,18 +153,23 @@ def main():
         return_attention_mask=True,
     )
 
+    model_name = args.model_path.split('/')[-2]
+    cfg_scale = 1.3
+    print(f"model name: {model_name}")
+
     # ==================================================================
     # start_time = time.time()
     # outputs = model.generate_negative_with_start_end_token(
     #     **inputs,
     #     max_new_tokens=None,
-    #     cfg_scale=1.3,
+    #     cfg_scale=cfg_scale,
     #     tokenizer=processor.tokenizer,
     # )
     # print(f"Generation time: {time.time() - start_time:.2f} seconds")
 
     # for i, save_name in enumerate(save_names):
-    #     output_path = f"/mnt/conversationhub/zhiliang/exp/podcast_eval/transformers_batch_generate_negative_with_start_end_token/{save_name}.wav"
+    #     output_path = f"/mnt/conversationhub/zhiliang/exp/podcast_eval/{model_name}/batch_negative_with_start_end_token_cfg_{cfg_scale}_step_5/{save_name}.wav"
+    #     # output_path = f"./vibepod_outputs/cfg_1.5_step_5_negative_with_SE_{save_name}.wav"
     #     os.makedirs(os.path.dirname(output_path), exist_ok=True)
     #     processor.save_audio(
     #         outputs.speech_outputs[i],
@@ -146,7 +182,7 @@ def main():
     # outputs = model.generate_negative_without_start_end_token(
     #     **inputs,
     #     max_new_tokens=None,
-    #     cfg_scale=1.3,
+    #     cfg_scale=cfg_scale,
     #     tokenizer=processor.tokenizer,
     # )
     # print(f"Generation time: {time.time() - start_time:.2f} seconds")
@@ -174,7 +210,7 @@ def main():
     #     sample_outputs = model.generate_negative_without_start_end_token(
     #         **sample_input,
     #         max_new_tokens=None,
-    #         cfg_scale=1.3,
+    #         cfg_scale=cfg_scale,
     #         tokenizer=processor.tokenizer,
     #     )
     #     output_path = f"/mnt/conversationhub/zhiliang/exp/podcast_eval/transformers_single_generate_negative_without_start_end_token/{sample_name}.wav"
@@ -191,13 +227,15 @@ def main():
     outputs = model.generate_refresh_negative(
         **inputs,
         max_new_tokens=None,
-        cfg_scale=1.3,
+        cfg_scale=cfg_scale,
         tokenizer=processor.tokenizer,
     )
     print(f"Generation time: {time.time() - start_time:.2f} seconds")
 
     for i, save_name in enumerate(save_names):
-        output_path = f"/mnt/conversationhub/zhiliang/exp/podcast_eval/transformers_batch_generate_refresh_negative/{save_name}.wav"
+        # output_path = f"/mnt/conversationhub/zhiliang/exp/podcast_eval/transformers_batch_generate_refresh_negative/{save_name}.wav"
+        # output_path = f"/mnt/conversationhub/zhiliang/exp/podcast_eval/{model_name}/batch_refresh_negative_cfg_{cfg_scale}_step_5/{save_name}.wav"
+        output_path = f"./vibepod_outputs/cfg_{cfg_scale}_{save_name}.wav"
         os.makedirs(os.path.dirname(output_path), exist_ok=True)
         processor.save_audio(
             outputs.speech_outputs[i],
@@ -206,30 +244,30 @@ def main():
         print(f"Saved output to {output_path}")
 
     # ==================================================================
-    start_time = time.time()
-    for sample_script, sample_voice, sample_name in zip(scripts, voice_samples, save_names):
-        print(f"Processing sample: {sample_name}")
-        sample_input = processor(
-            text=[sample_script],
-            voice_samples=[sample_voice],
-            padding=True,
-            return_tensors="pt",
-            return_attention_mask=True,
-        )
-        sample_outputs = model.generate_refresh_negative(
-            **sample_input,
-            max_new_tokens=None,
-            cfg_scale=1.3,
-            tokenizer=processor.tokenizer,
-        )
-        output_path = f"/mnt/conversationhub/zhiliang/exp/podcast_eval/transformers_single_generate_refresh_negative/{sample_name}.wav"
-        os.makedirs(os.path.dirname(output_path), exist_ok=True)
-        processor.save_audio(
-            sample_outputs.speech_outputs[0],
-            output_path=output_path,
-        )
-        print(f"Saved output to {output_path}")
-    print(f"Single sample generation time: {time.time() - start_time:.2f} seconds")
+    # start_time = time.time()
+    # for sample_script, sample_voice, sample_name in zip(scripts, voice_samples, save_names):
+    #     print(f"Processing sample: {sample_name}")
+    #     sample_input = processor(
+    #         text=[sample_script],
+    #         voice_samples=[sample_voice],
+    #         padding=True,
+    #         return_tensors="pt",
+    #         return_attention_mask=True,
+    #     )
+    #     sample_outputs = model.generate_refresh_negative(
+    #         **sample_input,
+    #         max_new_tokens=None,
+    #         cfg_scale=cfg_scale,
+    #         tokenizer=processor.tokenizer,
+    #     )
+    #     output_path = f"/mnt/conversationhub/zhiliang/exp/podcast_eval/transformers_single_generate_refresh_negative/{sample_name}.wav"
+    #     os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    #     processor.save_audio(
+    #         sample_outputs.speech_outputs[0],
+    #         output_path=output_path,
+    #     )
+    #     print(f"Saved output to {output_path}")
+    # print(f"Single sample generation time: {time.time() - start_time:.2f} seconds")
 
 
     # ==================================================================
@@ -247,7 +285,7 @@ def main():
     # sample_outputs = model.generate_refresh_negative(
     #     **sample_input,
     #     max_new_tokens=None,
-    #     cfg_scale=1.3,
+    #     cfg_scale=cfg_scale,
     #     tokenizer=processor.tokenizer,
     # )
     # processor.save_audio(
@@ -257,3 +295,8 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# /mnt/conversationhub/zhiliang/exp/sp_mllm/qwen_1.5b_stream_podcast_v2_4096_text-1_ddpm-diff-5_acous-seman-tok3200x64_lr1e-4_bsz4m_8n_100k/hf_ckpt
+# /mnt/conversationhub/zhiliang/exp/sp_mllm/qwen_1.5b_stream_en-zh-1-1_cfg0.1_12288_text-1_ddpm-diff-5_acous-seman-tok3200x64_lr1e-4_bsz3m_2n_40k/hf_ckpt_step10k
+# /mnt/conversationhub/zhiliang/exp/sp_mllm/qwen_1.5b_stream_en-zh-1-1_12288_text-1_ddpm-diff-5_acous-seman-tok3200x64_lr1e-4_bsz3m_2n_40k/hf_ckpt_step10k
+# /mnt/conversationhub/zhiliang/exp/sp_mllm/qwen_1.5b_stream_en-zh-1-1_12288_text-1_ddpm-diff-5_acous-seman-tok3200x64_lr1e-4_bsz3m_2n_40k/hf_ckpt_step40k
